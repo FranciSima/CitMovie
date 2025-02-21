2c91e622029471fd9051a125ad5428c3b7a28e79
kiltonpisli <kiltonpisli@gmail.com>
2025-02-21 21:33:13 +0100

    dockerifying the app

--- src/config.php
+++ src/config.php
@@ -1,7 +1,8 @@
 <?php
-    $host = "127.0.0.1";
+    // $host = "127.0.0.1";
+    $host = "db";
     $user = "root";                     
-    $pass = "";                                  
+    $pass = "1234";                                  
     $db = "movietheatredb";
     $port = 3306;
      $con = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
